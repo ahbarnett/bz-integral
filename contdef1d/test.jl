dd2d4bd5779bec88ec71f038a4414452d93963f4
ahbarnett <alex.h.barnett@gmail.com>
2022-12-06 13:20:51 -0500

    fixed BUG with hmconst = copy(hm)... ugh, Julia!

--- contdef1d/test.jl
+++ contdef1d/test.jl
@@ -27,7 +27,7 @@ function evalh(hm,x)
     Tout = eltype(complex(hm))
     h = zeros(Tout,size(x))
     for m in eachindex(hm)
-        h = h .+ hm[m]*exp.(1im*m*x)
+        h = h .+ hm[m]*exp.(im*m*x)
     end
     h
 end
@@ -42,16 +42,16 @@ function evalhp(hm,x)
     Tout = eltype(complex(hm))
     hp = zeros(Tout,size(x))
     for m in eachindex(hm)
-        hp = hp .+ hm[m]*1im*m*exp.(1im*m*x)
+        hp = hp .+ hm[m]*im*m*exp.(im*m*x)
     end
     hp
 end
 x=1.0; @printf "test evalh @ x=%g: " x; println(evalh(hm,x))
 # varinfo()
 
-η=1e-6      # broadening (quadgk obviously fails for small, below 1e-9)
-ω=0.5       # Fermi energy (band is [-1,1])
-f(x) = 1 ./ (evalh(hm,x) .+ (-ω+1im*η))     # integrand func
+η=1e-8      # broadening (quadgk obviously hangs for small, below 1e-9)
+ω=0.9       # Fermi energy (band is [-1,1])
+f(x) = 1 ./ (evalh(hm,x) .+ (-ω+im*η))     # integrand func
 
 ng = 1000    # Re axis plot grid
 g=2pi*(0:ng-1)./ng
@@ -63,9 +63,10 @@ fg = f(g)
 
 # ------------------------- Adaptive real quad method ----------------------
 
-Aa,err = quadgk(f,0,2π,rtol=1e-10)            # A via adaptive real integral
-@time Aa,err = quadgk(f,0,2π,rtol=1e-10)
-println("Aa adap quadgk:\t",Aa)              # we treat as reliable
+Aa,err = quadgk(f,0,2π,rtol=1e-8)            # A via adaptive real integral
+@printf "timing quadgk... "
+@time Aa,err = quadgk(f,0,2π,rtol=1e-8)
+println("Aa adap quadgk:\t",Aa)              # for eta<1e-8, only good to 1e-7
 
 # -------------------------- Deformed contour method ------------------------
 """
@@ -73,24 +74,27 @@ println("Aa adap quadgk:\t",Aa)              # we treat as reliable
 
     Apply N-node PTR to quadrature of f from 0 to 2π along contour
     Im z = Cfun(Re z). Cfunp must be derivative function of Cfun.
+    Returns integral, and the complex nodes used zj.
     To do: * remove need for Cfunp via spectral diff of Cfun on grid.
 """
 function contourPTR(f::Function,Cfun::Function,Cfunp::Function,N::Int)
     tj=2π*(1:N)/N
-    zj = tj .+ 1im*Cfun.(tj)
-    wj = (1 .+ 1im*Cfunp.(tj)) * (2π/N)
+    zj = tj .+ im*Cfun.(tj)
+    wj = (1 .+ im*Cfunp.(tj)) * (2π/N)
     return sum(wj.*f.(zj)), zj
 end
-# Cfun, Cfunp = x->0.0.*x, x->0.0.*x; Ac,zj = contourPTR(f,Cfun,Cfunp,1000) # dumb Re test
-Cfun, Cfunp = x->-sin(x), x->-cos(x)   # a contour good for |ω|<1
-Ac,zj = contourPTR(f,Cfun,Cfunp,100)
-Ac2,zj2 = contourPTR(f,Cfun,Cfunp,200)   # check
-@printf "contour PTR diff:        \t%.3g\n" abs(Ac-Ac2)
+# Cfun, Cfunp = x->0.0.*x, x->0.0.*x; Ac,zj = contourPTR(f,Cfun,Cfunp,1000) # dumb on-Re test
+Cfun, Cfunp = x->-sin(x), x->-cos(x)   # a contour good for |ω|<1, dies -> 1.
+# add more PTR as poles coalesce...
+N=maximum([50,ceil(Int,80/sqrt(1-ω^2))]); @printf "contour PTR N=%d\n" N
+Ac,zj = contourPTR(f,Cfun,Cfunp,N)
+Ac2,zj2 = contourPTR(f,Cfun,Cfunp,2N)   # self-conv
+@printf "contour PTR self-conv:   \t%.3g\n" abs(Ac-Ac2)
 @printf "contour PTR diff |Ac-Aa|:\t%.3g\n" abs(Ac-Aa)
 
 dx=0.03       # C plane plot grid
 gx=range(0.0,2π,step=dx); gy=range(-1.0,1.0,step=dx)
-gz = gx .+ 1im*gy'    # 2d array x-by-y (NB gnuplot flipped vs MATLAB)
+gz = gx .+ im*gy'    # 2d array x-by-y (NB gnuplot flipped vs MATLAB)
 fz = f(gz)            # func needs to preserve size
 sc = 10               # color scale
 @gp :C gx gy imag(fz) "w image notit" "set size ratio -1" palette(:jet1)
@@ -135,22 +139,28 @@ roots(complex([1.0,0,1.0]))       # complex case
 roots([1.0])  # empty list
 roots([0.0])  # all C-#s
 
-hmconst = hm; hmconst[0] = hm[0] +  (-ω+1im*η)   # F series for denominator
+hmconst = copy(hm);           # crucial! otherwise changing hmconst changes hm!
+hmconst[0] = hm[0] + (-ω+im*η)     # F series for denominator
 zr = roots(hmconst[M:-1:-M])  # shift powers by M (must be std vec; reverse not)
-xr = @. log(zr)/1im           # solve z = e^{ix}
-xr = @. mod(real(xr),2π) + 1im*imag(xr)    # fold Re parts of roots into [0,2π)
+xr = @. log(zr)/im            # solve z = e^{ix}
+xr = @. mod(real(xr),2π) + im*imag(xr)   # fold Re parts into [0,2π), for humans
 println("poles in x plane: ", xr)
+@printf "largest denom mag at any pole (checks roots worked): %.3g\n" maximum(abs.(evalh(hm,xr) .+ (-ω+im*η)))
 @gp :C :- real(xr) imag(xr) "w p pt 7 ps 1.0 lc '#ffffff' t 'poles'"
 
 Cimsh = 1.0                 # contour imag const shift
 Cfun, Cfunp = x->0*x.+Cimsh, x->0*x
 Ar,zjr = contourPTR(f,Cfun,Cfunp,50)
+@printf "Ar on shifted contour = %g+%gi\n" real(Ar) imag(Ar)
 resr = 1.0 ./ evalhp(hm,xr)     # residues of f at all poles ( = 1/h'(x_r))
 for i in eachindex(xr)
-    if imag(xr[i]) .> 0.0       # just the poles that contour passed through
+    if 0.0 < imag(xr[i]) < 1.0   # just the poles that contour passed through
         @printf "correcting res %g+%gi at pole %g+%gi...\n" real(resr[i]) imag(resr[i]) real(xr[i]) imag(xr[i])
-        global Ar = Ar + 2π*1im*resr[i]   # correct by residue formula
+        global Ar = Ar + 2π*im*resr[i]   # correct by residue formula
     end
 end
-@printf "Res-corr PTR diff |Ar-Aa|:\t%.3g\n" abs(Ar-Ac)
-@gp :C :- real(zjr) imag(zjr) "w p pt 6 ps 0.3 lc '#000000' t 'Res-corr nodes'"
+@printf "Res-corr PTR diff |Ar-Aa|: \t%.3g\n" abs(Ar-Aa)
+@printf "Res-corr PTR diff Im Ar-Aa:\t%.3g\n" imag(Ar-Aa)
+@printf "Res-corr PTR diff |Ar-Ac|: \t%.3g\n" abs(Ar-Ac)
+@printf "Res-corr PTR diff Im Ar-Ac:\t%.3g\n" imag(Ar-Ac)
+@gp :C :- real(zjr) imag(zjr) "w p pt 6 ps 0.7 lc '#000000' t 'Res-corr nodes'"
