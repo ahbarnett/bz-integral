1bac744908d8c577e78d02346ad18fcf16b4a0c4
ahbarnett <alex.h.barnett@gmail.com>
2023-01-19 18:24:28 -0500

    tweaks to test_Cont1DBZ

--- contdef1d/test_Cont1DBZ.jl
+++ contdef1d/test_Cont1DBZ.jl
@@ -1,4 +1,5 @@
-# unit tester for contour 1D BZ module in this dir. Barnett 12/19/22
+# unit tester for contour 1D BZ module in this dir.
+# Barnett 12/19/22. LXVM added fourier_kernel which is an evalh replacement.
 
 push!(LOAD_PATH,".")
 using Cont1DBZ
@@ -21,15 +22,16 @@ for t=1:length(xtest)
     if t==1
         @printf "\tevalh @ x=%g: " x; println(evalh(hm,x))
     end
-    @printf "evalh chk: %g\n" norm(evalh(hm,x) .- evalh_ref(hm,x),Inf)
-    @printf "evalh_wind chk: %g\n" norm(evalh_wind(hm,x) .- evalh_ref(hm,x),Inf)
-    @printf "fourier_kernel chk: %g\n" norm(fourier_kernel.(Ref(hm),x) .- evalh_ref(hm,x),Inf)
+    @printf "evalh chk:          %.3g\n" norm(evalh(hm,x) .- evalh_ref(hm,x),Inf)
+    @printf "evalh_wind chk:     %.3g\n" norm(evalh_wind(hm,x) .- evalh_ref(hm,x),Inf)
+    @printf "fourier_kernel chk: %.3g\n" norm(fourier_kernel.(Ref(hm),x) .- evalh_ref(hm,x),Inf)
 end
 
 η=1e-6; ω=0.5; tol=1e-8;
 @printf "test realadap for M=%d ω=%g η=%g tol=%g...\n" M ω η tol
 Aa = realadap(hm,ω,η,tol=tol, verb=1)
-    
+
+@printf "Now test just our roots method (also see bench_roots.jl)...\n"
 @testset "roots" begin
 r = roots([1.0,0,1.0])                # real coeffs case
 @test maximum(abs.(r).-1.0) < 1e-14 && (sort(angle.(r)) ≈ [-pi/2,pi/2])  # +-i
@@ -45,7 +47,7 @@ end
 NPTR=30
 Ac = imshcorr(hm,ω,η,N=NPTR, verb=0)         # a=1 so Davis exp(-aN) ~ 1e-13
 #println(Ac,' ',Aa)
-@printf "test imshcorr, N_PTR=%d:   \t|Ac-Aa| = %g\n" NPTR abs(Ac-Aa)
+@printf "test imshcorr, N_PTR=%d:   \t|Ac-Aa| = %.3g    (don't trust below claimed err)\n" NPTR abs(Ac-Aa)
 # known band struc case with eta=0+
 M=1; hm = OffsetVector(complex([1/2,0,1/2]),-M:M)    # h(x)=cos(x), band [-1,1]
 ωs=[-1.6, -0.3, 0.9, 1.3, 1.9]          # below, thru, above band
@@ -54,7 +56,7 @@ for ω in ωs
     local Ac = imag(imshcorr(hm,ω,η,N=NPTR))   # Im for DOS
     Aanal = abs(ω)<1 ? 2π/sqrt(1-ω^2) : 0.0    # formula; DOS=0 outside band
     #println(Ac,' ',Aa)
-    @printf "η=0+ Im test ω=%g:     \t|Ac-Aanal| = %g\n" ω abs(Ac-Aanal)
+    @printf "η=0+ Im test ω=%g:     \t|Ac-Aanal| = %.3g\n" ω abs(Ac-Aanal)
 end
 
 # ------------ end module tests---------------
