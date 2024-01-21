da32e7d2fd3c060f618941d29fd8c3645dbad172
ahbarnett <alex.h.barnett@gmail.com>
2024-01-20 23:40:18 -0500

    test genchebquad

--- Int1DBZ/test/test_genchebquad.jl
+++ Int1DBZ/test/test_genchebquad.jl
@@ -1,4 +1,4 @@
-# test script for generalized Chebyshev quadratures
+# test script for generalized Chebyshev quadratures. Barnett 1/20/24
 using Int1DBZ    # for miniquadgk
 using Printf
 using GLMakie
@@ -7,24 +7,25 @@ include("../src/genchebquad.jl")
 
 function testquadfuncset(x::Vector, w::Vector, fs::Function,a,b,reqtol)
 # make sure function set fs is integrated by rule (x,w) to requested tol
+# returns the vector of errors of the rule for each function in the set.
     I,_,_,nev = miniquadgk(fs,a,b;rtol=1e-14,atol=1e-14);   # estim true i 
     Ig = reduce(hcat,fs.(x)) * w;    # test rule (x,w). reduce makes Nf*n mat (n=#nodes)
     maxerr = norm(I-Ig,Inf)
     @printf "max abs I err in fs vs miniquadgk(%d evals) = %.3g (vs tol=%.3g)\n" nev maxerr tol
     if maxerr<10tol; println("passed."); else println("failed!"); end
-    maxerr
+    I-Ig
 end
 
 tol=1e-12
+for case = 0:2     # ---------------------------------- loop over func set choices
 
-case = 0
 if case==0; println("Trivial test on monomials...")
     a=-1.0; b=1.0
     fs(x::Number) = [x^k for k=0:20]
 elseif case==1; println("Basic test on non-integer power set...")
     a=0.0; b=1.0
     fs(x::Number) = [x^r for r=-0.53.+0.29*(0:30)]  #r=0.63*(-1:30)]     # set of irrational integrable powers >-1
-    # *** note: fails for r>0.6 :(
+    # *** note: fails for min r<-0.6. not sure why - try arb prec?
 elseif case==2; println("poly plus nearby complex sqrt times poly...")
     a=-1.0; b=1.0
     p=20            # max degree
@@ -39,7 +40,7 @@ elseif case==2; println("poly plus nearby complex sqrt times poly...")
 end
 
 x, w, i = genchebquad(fs,a,b,tol;verb=1)
-testquadfuncset(x,w,fs,a,b,tol)        # check the rule
+Ierrs = testquadfuncset(x,w,fs,a,b,tol)        # check the rule
 
 if case!=1
     f(x) = sin(1+3x); fp(x) = 3*cos(1+3x)   # fp must be deriv of f
@@ -51,8 +52,8 @@ if case==2
 end
 
 if true              # plot stuff from info struct i
-    GLMakie.closeall()
-    GLMakie.activate!(title="test genchebquad");
+    #GLMakie.closeall()
+    GLMakie.activate!(title=@sprintf "test genchebquad case=%d" case);
     fig=Figure(); ax1=Axis(fig[1,1],title="input func set")
     Nf = length(fs(a));   # num input funcs
     t=range(a,b,1000); F=reduce(hcat,fs.(t));  # eval all fs to plot
@@ -67,8 +68,11 @@ if true              # plot stuff from info struct i
     linesegments!(kron(x,[1;1]),kron(w,[0;1])) # stick plot
     ax4=Axis(fig[2,2], yscale=log10, limits=(nothing,(1e-16,1)),
         title="abs I err over func set")
-    scatter!(abs.(I-Ig))
-    lines!([0,Nf],[tol,tol],color=:red)
-    display(fig)
-    #display(GLMakie.Screen(),fig)       # new window
+    scatter!(abs.(Ierrs))
+    lines!([0,Nf],[tol,tol],color=:red,label="tol")
+    axislegend()
+    #display(fig)
+    display(GLMakie.Screen(),fig)       # new window
 end
+
+end                        # -------------------------------------------
